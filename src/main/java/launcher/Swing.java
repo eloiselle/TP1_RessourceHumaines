533b37e926c821c958d1b3f7d879e804a2b2ee35
eloiselle <emile.loiselle@hotmail.com>
2019-03-12 08:53:46 -0400

    Auto stash before merge of "emile" and "origin/emile"

--- src/main/java/launcher/Swing.java
+++ src/main/java/launcher/Swing.java
@@ -1,21 +1,68 @@
 package launcher;
 
+import java.awt.Color;
+import java.io.IOException;
+
 import javax.swing.*;
+import net.miginfocom.swing.MigLayout;
 
 public class Swing {
-    
-    
-    public static void main(String[] arguments) {
-        
-        JLabel label = new JLabel("Hello World");
-        
-        JFrame.setDefaultLookAndFeelDecorated(true);
-        JFrame f = new JFrame("Hello World");
-        f.setSize(300, 150);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        f.add(label);
-        
-        f.setVisible(true);
-    }
+
+	public static void main(String[] arguments) throws IOException {
+
+		JLabel lblOffreEmploi = new JLabel("Offre d'emploi");
+		JLabel lblCandidat = new JLabel("Candidat");
+		JLabel lblTitreEmploi = new JLabel("titreEmploi");
+		JLabel lblNomPrenom = new JLabel("Nom, Prénom");
+
+		/*lblOffreEmploi.setBackground(Color.BLUE);
+		lblOffreEmploi.setOpaque(true);*/
+		
+		/* JFrame.setDefaultLookAndFeelDecorated(true); */
+		JFrame f = new JFrame("Gestion des offres d'emplois");
+		f.setLayout(new MigLayout(
+				"",
+				"[40][150!][200!][150!][80!][150!][200!][150!][40]",
+				"[grow][20!][25!][25!][20!][300!][100!][grow]"));
+		
+		f.setBackground(Color.BLACK);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		f.add(lblOffreEmploi, "cell 2 2, center");
+		f.add(lblCandidat, "cell 6 2, center");
+		f.add(lblTitreEmploi, "cell 2 3, center");
+		f.add(lblNomPrenom, "cell 6 3, center");
+		
+		f.pack();
+		f.setVisible(true);
+	}
+
+	/*private static void createAndShowGUI() {
+
+		// button.setActionCommand("Submit");
+		// button.addActionListener(new ButtonActionListener());
+
+		// Added to the frame
+		frame.getContentPane().add(lblImagerySet, "align right");
+		frame.getContentPane().add(txtFldImagerySet, "wrap");
+
+		frame.getContentPane().add(lblCenterPoint, "align right");
+		frame.getContentPane().add(txtFldCenterPoint, "wrap");
+
+		frame.getContentPane().add(lblOrientation, "align right");
+		frame.getContentPane().add(txtFldOrientation, "wrap");
+
+		frame.getContentPane().add(lblZoomLvl, "align right");
+		frame.getContentPane().add(txtFldZoomLvl, "wrap");
+
+		frame.getContentPane().add(lblBingMapsAPIKey, "align right");
+		frame.getContentPane().add(txtFldBingMapsAPIKey, "wrap");
+
+		frame.getContentPane().add(button, "span, grow");
+
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}*/
+
 }
